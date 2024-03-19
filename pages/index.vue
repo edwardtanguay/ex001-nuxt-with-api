155e1ff2c0af1e20fd21eccb489b06cb6f797bb8
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-19 14:44:22 +0100

    base works

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,6 @@
 <script setup lang="ts">
-const message = ref("");
 import { appStore } from "~/stores/AppStore";
-
-// const appName = useState("appName", () => "Nuxt Learning Showcase 2.0");
-// console.log("appName", appName);
-
-message.value =
-	"This site is a application to try out, experiment with and showcase Nuxt features as I learn the framework.";
 </script>
 <template>
-	<NuxtLayout>
-		<!-- <p>appName: {{ appName }}</p> -->
-		<p>{{ message }}</p>
-		<DevOnly>
-			<div class="bg-gray-400 p-6 w-fit mt-6 rounded font-bold">
-				This is developer information inside the DevOnly element. It
-				won't be shown in production.
-			</div>
-		</DevOnly>
-		<p class="mt-4">appMessage: {{ appStore.appMessage }}</p>
-	</NuxtLayout>
+	<p class="mt-4">appMessage: {{ appStore.appMessage }}</p>
 </template>
